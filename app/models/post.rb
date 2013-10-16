84d1c3418345244143688592056cd8a58cac1783
Radek Dul <radek@duldul.pl>
2013-10-17 00:01:52 +0200

    hotness rank

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,4 +17,15 @@ class Post
   def archive!
     update_attribute :archived, true
   end
+
+  def hotness
+    rating = case (Time.now-created_at)
+      when 3.days .. 7.days then 1
+      when 24.hours .. 72.hours then 2
+      when 0 .. 24.hours then 3
+      else 0
+    end
+
+    self.comments.count > 3 ? rating+1 : rating
+  end
 end
